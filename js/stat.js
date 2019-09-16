a4f24bf5625bb7d18ee03b453971a5f42094bb13
Anton <933375@gmail.com>
2019-09-16 21:50:08 +0300

    исправил ошибки в оформлении

--- js/stat.js
+++ js/stat.js
@@ -1,4 +1,4 @@
-"use strict"
+'use strict';
 
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
@@ -14,39 +14,39 @@ var columnOriginY = CLOUD_Y + CLOUD_HEIGHT - GAP * 2;
 
 var renderCloud = function renderCloud(ctx, color, x, y) {
   ctx.fillStyle = color;
-  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT)
-}
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
 
 var maxElement = function getMaxElement(arr) {
   maxElement = arr[0];
-  for (var i=1; i < arr.length; i++) {
+  for (var i = 1; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
   }
-  return maxElement
-}
+  return maxElement;
+};
 
 
 window.renderStatistics = function renderStatistics(ctx, names, times) {
   var maxTime = maxElement(times);
 
-  renderCloud(ctx, "rgba(0, 0, 0, 0.7)", CLOUD_X + GAP, CLOUD_Y + GAP);
-  renderCloud(ctx, "#ffffff", CLOUD_X, CLOUD_Y);
+  renderCloud(ctx, 'rgba(0, 0, 0, 0.7)', CLOUD_X + GAP, CLOUD_Y + GAP);
+  renderCloud(ctx, '#ffffff', CLOUD_X, CLOUD_Y);
 
-  ctx.fillStyle = "black";
-  ctx.font = "16px PT Mono";
-  ctx.fillText("Ура вы победили!", CLOUD_X + GAP * 2, CLOUD_Y + GAP + FONT_GAP);
-  ctx.fillText("Список результатов:", CLOUD_X + GAP * 2, CLOUD_Y + GAP + FONT_GAP * 2);
+  ctx.fillStyle = 'black';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAP * 2, CLOUD_Y + GAP + FONT_GAP);
+  ctx.fillText('Список результатов:', CLOUD_X + GAP * 2, CLOUD_Y + GAP + FONT_GAP * 2);
 
 
   for (var i = 0; i < names.length; i++) {
     var opacity = Number(Math.random().toFixed(2));
 
-    ctx.fillStyle = "black";
+    ctx.fillStyle = 'black';
     ctx.fillText(names[i], columnOriginX + (COLUMN_WIDTH + COLUMN_GAP) * i, columnOriginY);
     ctx.fillText(times[i].toFixed(0), columnOriginX + (COLUMN_WIDTH + COLUMN_GAP) * i, columnOriginY - FONT_GAP - BAR_HEIGHT * times[i] / maxTime - GAP);
-    ctx.fillStyle = ((names[i] == "Вы") ? "rgba(255, 0, 0, 1)" : "hsla(252, 100%, 50%, " + opacity + ")");
+    ctx.fillStyle = ((names[i] === 'Вы') ? 'rgba(255, 0, 0, 1)' : 'hsla(252, 100%, 50%, ' + opacity + ')');
     ctx.fillRect(columnOriginX + (COLUMN_WIDTH + COLUMN_GAP) * i, columnOriginY - FONT_GAP, COLUMN_WIDTH, -BAR_HEIGHT * times[i] / maxTime);
   }
-}
+};
