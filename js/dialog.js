7aedd8acfdc7a0253070ff102b37a9d273e6a2d0
Anton <933375@gmail.com>
2019-10-03 19:03:21 +0300

    разделил код на модули

--- js/dialog.js
+++ js/dialog.js
@@ -1,213 +1,234 @@
 'use strict';
+(function () {
+  var FIREBALL_COLOR = [
+    'rgb(238, 72, 48)',
+    'rgb(48, 168, 238)',
+    'rgb(92, 230, 192)',
+    'rgb(232, 72, 213)',
+    'rgb(230, 232, 72)'
+  ];
 
-var KEY_ENTER = 13;
-var KEY_ESCAPE = 27;
+  var NEXT_COLOR_INDEX = 1;
 
+  var dragged = false;
 
-/**
- * increment index within length of list
- * @param {number} currentIndex
- * @param {Array} listOfElements
- * @return {number}
- */
-function changeIndex(currentIndex, listOfElements) {
-  var newIndex;
-  if (currentIndex === listOfElements.length - 1) {
-    newIndex = 0;
-  } else {
-    newIndex = currentIndex + 1;
-  }
-  return newIndex;
-}
-
-/**
- * set next fill value of an element from a list
- * @param {object} element
- * @param {array} featureList
- */
-function setFillAttr(element, featureList) {
-  var attrValue = element.getAttribute('style');
-  var currentColor;
-  var currentIndex;
-  var newIndex;
-  var newColor;
-
-  if (attrValue === null) {
-    newColor = newColor = featureList[NEXT_COLOR_INDEX];
-  } else {
-    currentColor = attrValue.slice(6, attrValue.length);
-    currentIndex = featureList.indexOf(currentColor);
-    newIndex = changeIndex(currentIndex, featureList);
-    newColor = featureList[newIndex];
+  /**
+   * increment index within length of list
+   * @param {number} currentIndex
+   * @param {Array} listOfElements
+   * @return {number}
+   */
+  function changeIndex(currentIndex, listOfElements) {
+    var newIndex;
+    if (currentIndex === listOfElements.length - 1) {
+      newIndex = 0;
+    } else {
+      newIndex = currentIndex + 1;
+    }
+    return newIndex;
   }
 
-  element.setAttribute('style', 'fill: ' + newColor);
-}
+  /**
+   * set next fill value of an element from a list
+   * @param {object} element
+   * @param {array} featureList
+   */
+  function setFillAttr(element, featureList) {
+    var attrValue = element.getAttribute('style');
+    var currentColor;
+    var currentIndex;
+    var newIndex;
+    var newColor;
 
-/**
- * set next background-color value of an element from a list
- * @param {object} element
- * @param {array} featureList
- */
-function setBgAttr(element, featureList) {
-  var currentColor = element.style.backgroundColor;
-  var currentIndex;
-  var newIndex;
-  var newColor;
+    if (attrValue === null) {
+      newColor = featureList[NEXT_COLOR_INDEX];
+    } else {
+      currentColor = attrValue.slice(6, attrValue.length);
+      currentIndex = featureList.indexOf(currentColor);
+      newIndex = changeIndex(currentIndex, featureList);
+      newColor = featureList[newIndex];
+    }
 
-  if (currentColor === '') {
-    newColor = featureList[NEXT_COLOR_INDEX];
-  } else {
-    currentIndex = featureList.indexOf(currentColor);
-    newIndex = changeIndex(currentIndex, featureList);
-    newColor = featureList[newIndex];
+    element.setAttribute('style', 'fill: ' + newColor);
   }
 
-  element.style.backgroundColor = newColor;
-}
+  /**
+   * set next background-color value of an element from a list
+   * @param {object} element
+   * @param {array} featureList
+   */
+  function setBgAttr(element, featureList) {
+    var currentColor = element.style.backgroundColor;
+    var currentIndex;
+    var newIndex;
+    var newColor;
+
+    if (currentColor === '') {
+      newColor = featureList[NEXT_COLOR_INDEX];
+    } else {
+      currentIndex = featureList.indexOf(currentColor);
+      newIndex = changeIndex(currentIndex, featureList);
+      newColor = featureList[newIndex];
+    }
+
+    element.style.backgroundColor = newColor;
+  }
+
+  /**
+   * set a color of the coat by click
+   */
+  function wizardCoatClickHandler() {
+    setFillAttr(setupWizardCoat, window.setup.COAT_COLORS);
+  }
+
+  /**
+   * set a color of the eyes by click
+   */
+  function wizardEyesClickHandler() {
+    setFillAttr(setupWizardEyes, window.setup.EYE_COLORS);
+  }
+
+  /**
+   * set a color of the fireball by click
+   */
+  function wizardFireballClickHandler() {
+    setBgAttr(setupWizardFireball, FIREBALL_COLOR);
+  }
+
+  function dialogHandlePicMouseDownHandler(evt) {
+    evt.preventDefault();
+    var origin = {
+      'x': evt.clientX,
+      'y': evt.clientY
+    };
+
+    function mouseMoveHandler(moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+
+      var shift = {
+        'x': origin.x - moveEvt.clientX,
+        'y': origin.y - moveEvt.clientY,
+      };
+
+      origin = {
+        'x': moveEvt.clientX,
+        'y': moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    }
+
+    function mouseUpHandler(upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', mouseMoveHandler);
+      document.addEventListener('mousemove', mouseUpHandler);
+
+      if (dragged) {
+        dialogHandle.addEventListener('click', function preventDefaultClickHandler(clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandle.removeEventListener('click', preventDefaultClickHandler);
+          dragged = false;
+        });
+      }
+    }
+
+    document.addEventListener('mousemove', mouseMoveHandler);
+    document.addEventListener('mouseup', mouseUpHandler);
+
+  }
+
+  /**
+   * close the popup
+   */
+  function closePopup() {
+    setup.classList.add('hidden');
+    setup.removeAttribute('style');
+    document.removeEventListener('keydown', popupKeydownEscHandler);
+    closeButtonSetup.addEventListener('click', closeSetupClickHandler);
+    closeButtonSetup.addEventListener('keydown', closeSetupKeydownEnterHandler);
+
+    setupWizardCoat.removeEventListener('click', wizardCoatClickHandler);
+    setupWizardEyes.removeEventListener('click', wizardEyesClickHandler);
+    setupWizardFireball.removeEventListener('click', wizardFireballClickHandler);
+
+    dialogHandle.addEventListener('mousedown', dialogHandlePicMouseDownHandler);
+  }
+
+  /**
+   * close popup by click
+   */
+  function closeSetupClickHandler() {
+    closePopup();
+  }
+
+  /**
+   * close popup by press Enter
+   * @param {*} evt
+   */
+  function closeSetupKeydownEnterHandler(evt) {
+    window.util.isEnterEvent(evt, closePopup);
+
+  }
+
+  /**
+   * close popup by press Esc
+   * @param {*} evt
+   */
+  function popupKeydownEscHandler(evt) {
+    if (document.activeElement !== userNameSetup) {
+      window.util.isEscEvent(evt, closePopup);
+    }
+  }
 
 
-/**
- * open the popup
- */
-function openPopup() {
-  setup.classList.remove('hidden');
-  document.addEventListener('keydown', popupKeydownEscHandler);
-}
+  /**
+   * open the popup
+   */
+  function openPopup() {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', popupKeydownEscHandler);
+    closeButtonSetup.addEventListener('click', closeSetupClickHandler);
+    closeButtonSetup.addEventListener('keydown', closeSetupKeydownEnterHandler);
 
-/**
- * close the popup
- */
-function closePopup() {
-  setup.classList.add('hidden');
-  setup.removeAttribute('style');
-  document.removeEventListener('keydown', popupKeydownEscHandler);
+    setupWizardCoat.addEventListener('click', wizardCoatClickHandler);
+    setupWizardEyes.addEventListener('click', wizardEyesClickHandler);
+    setupWizardFireball.addEventListener('click', wizardFireballClickHandler);
 
-  setupWizardCoat.removeEventListener('click', wizardCoatClickHandler);
-  setupWizardEyes.removeEventListener('click', wizardEyesClickHandler);
-  setupWizardFireball.removeEventListener('click', wizardFireballClickHandler);
-}
+    dialogHandle.addEventListener('mousedown', dialogHandlePicMouseDownHandler);
+  }
 
-/**
- * open popup by click
- */
-function openSetupClickHandler() {
-  openPopup();
-}
-
-/**
- * open popup by press Enter
- * @param {*} evt
- */
-function OpenSetupKeydownEnterHandler(evt) {
-  if (evt.keyCode === KEY_ENTER) {
+  /**
+   * open popup by click
+   */
+  function openSetupClickHandler() {
     openPopup();
   }
-}
 
-/**
- * close popup by click
- */
-function closeSetupClickHandler() {
-  closePopup();
-}
-
-/**
- * close popup by press Enter
- * @param {*} evt
- */
-function closeSetupKeydownEnterHandler(evt) {
-  if (evt.keyCode === KEY_ENTER) {
-    closePopup();
-  }
-}
-
-
-/**
- * close popup by press Esc
- * @param {*} evt
- */
-function popupKeydownEscHandler(evt) {
-  if (document.activeElement !== userNameSetup && evt.keyCode === KEY_ESCAPE) {
-    closePopup();
-  }
-}
-
-/**
- * set a color of the coat by click
- */
-function wizardCoatClickHandler() {
-  setFillAttr(setupWizardCoat, COAT_COLORS);
-}
-
-/**
- * set a color of the eyes by click
- */
-function wizardEyesClickHandler() {
-  setFillAttr(setupWizardEyes, EYE_COLORS);
-}
-
-/**
- * set a color of the fireball by click
- */
-function wizardFireballClickHandler() {
-  setBgAttr(setupWizardFireball, FIREBALL_COLOR);
-}
-
-function dialogHandlePicMouseDownHandler(evt) {
-  evt.preventDefault();
-  var origin = {
-    'x': evt.clientX,
-    'y': evt.clientY
-  };
-
-  function mouseMoveHandler(moveEvt) {
-    moveEvt.preventDefault();
-    var shift = {
-      'x': origin.x - moveEvt.clientX,
-      'y': origin.y - moveEvt.clientY,
-    };
-
-    origin = {
-      'x': moveEvt.clientX,
-      'y': moveEvt.clientY
-    };
-
-    setup.style.top = (setup.offsetTop - shift.y) + 'px';
-    setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+  /**
+   * open popup by press Enter
+   * @param {*} evt
+   */
+  function OpenSetupKeydownEnterHandler(evt) {
+    window.util.isEnterEvent(evt, openPopup);
   }
 
-  function mouseUpHandler(upEvt) {
-    document.removeEventListener('mousemove', mouseMoveHandler);
-    document.addEventListener('mousemove', mouseUpHandler);
-  }
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupOpenIcon = document.querySelector('.setup-open-icon');
 
-  document.addEventListener('mousemove', mouseMoveHandler);
-  document.addEventListener('mouseup', mouseUpHandler);
+  var closeButtonSetup = setup.querySelector('.setup-close');
+  var userNameSetup = setup.querySelector('.setup-user-name');
 
-}
+  var setupWizardCoat = document.querySelector('.wizard-coat');
+  var setupWizardEyes = document.querySelector('.wizard-eyes');
+  var setupWizardFireball = document.querySelector('.setup-fireball-wrap');
 
+  var dialogHandle = setup.querySelector('.upload');
 
-var setupOpen = document.querySelector('.setup-open');
-var setupOpenIcon = document.querySelector('.setup-open-icon');
+  setupOpen.addEventListener('click', openSetupClickHandler);
+  setupOpenIcon.addEventListener('keydown', OpenSetupKeydownEnterHandler);
+})();
 
-var closeButtonSetup = setup.querySelector('.setup-close');
-var userNameSetup = setup.querySelector('.setup-user-name');
-
-var setupWizardCoat = document.querySelector('.wizard-coat');
-var setupWizardEyes = document.querySelector('.wizard-eyes');
-var setupWizardFireball = document.querySelector('.setup-fireball-wrap');
-
-var dialogHandle = setup.querySelector('.upload');
-
-setupOpen.addEventListener('click', openSetupClickHandler);
-setupOpenIcon.addEventListener('keydown', OpenSetupKeydownEnterHandler);
-
-closeButtonSetup.addEventListener('click', closeSetupClickHandler);
-closeButtonSetup.addEventListener('keydown', closeSetupKeydownEnterHandler);
-
-setupWizardCoat.addEventListener('click', wizardCoatClickHandler);
-setupWizardEyes.addEventListener('click', wizardEyesClickHandler);
-setupWizardFireball.addEventListener('click', wizardFireballClickHandler);
-
-dialogHandle.addEventListener('mousedown', dialogHandlePicMouseDownHandler);
--- js/setup.js
+++ js/setup.js
@@ -1,129 +1,107 @@
 'use strict';
-// объявление констант
+(function () {
+  // объявление констант
 
-var FIRST_NAMES = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон'
-];
-var LAST_NAMES = [
-  'да Марья',
-  'Верон',
-  'Мирабелла',
-  'Вальц',
-  'Онопко',
-  'Топольницкая',
-  'Нионго',
-  'Ирвинг'
-];
-var COAT_COLORS = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
-var EYE_COLORS = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green'
-];
+  var FIRST_NAMES = [
+    'Иван',
+    'Хуан Себастьян',
+    'Мария',
+    'Кристоф',
+    'Виктор',
+    'Юлия',
+    'Люпита',
+    'Вашингтон'
+  ];
+  var LAST_NAMES = [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг'
+  ];
+  var COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
+  var EYE_COLORS = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
 
-var FIREBALL_COLOR = [
-  'rgb(238, 72, 48)',
-  'rgb(48, 168, 238)',
-  'rgb(92, 230, 192)',
-  'rgb(232, 72, 213)',
-  'rgb(230, 232, 72)'
-];
+  var NUMBER_OF_WIZARD = 4;
 
-var NEXT_COLOR_INDEX = [1];
+  window.setup = {
+    COAT_COLORS: COAT_COLORS,
+    EYE_COLORS: EYE_COLORS
+  };
 
-var PROPERTIES = {
-  '.setup-similar-label': 'name',
-  '.wizard-coat': 'coatColors',
-  '.wizard-eyes': 'eyesColors'
-};
+  // описание функций
 
-var NUMBER_OF_WIZARD = 4;
+  /**
+   * Returns random element of an array.
+   * @param {array} arr collection of elements.
+   * @return {string} random element from the collection.
+   */
+  function chooseRandom(arr) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
 
-// описание функций
+  /**
+   * Returns generated name of a wizard
+   * @return {object} an object that contains wizard's name and colors of eyes and a coat.
+   */
+  function generateWizard() {
+    var wizard = {};
 
-/**
- * Returns random element of an array.
- * @param {array} arr collection of elements.
- * @return {string} random element from the collection.
- */
-function chooseRandom(arr) {
-  return arr[Math.floor(Math.random() * arr.length)];
-}
+    wizard.name = chooseRandom(FIRST_NAMES) + ' ' + chooseRandom(LAST_NAMES);
+    wizard.coatColors = chooseRandom(COAT_COLORS);
+    wizard.eyesColors = chooseRandom(EYE_COLORS);
 
-/**
- * Returns generated name of a wizard
- * @return {object} an object that contains wizard's name and colors of eyes and a coat.
- */
-function generateWizard() {
-  var wizard = {};
+    return wizard;
+  }
 
-  wizard.name = chooseRandom(FIRST_NAMES) + ' ' + chooseRandom(LAST_NAMES);
-  wizard.coatColors = chooseRandom(COAT_COLORS);
-  wizard.eyesColors = chooseRandom(EYE_COLORS);
+  /**
+   * made a set of DOM's object from the data and from template
+   * @param {object} wizard
+   * @return {object} DOM's element 'wizard'.
+   */
 
-  return wizard;
-}
-
-/**
- * made a set of DOM's object from the data and from template
- * @param {object} template a template of DOM's object.
- * @param {array} data a list of objects for writing to template.
- * @param {array} properties an object which shows correspondence between the data and template's properties.
- * @return {object} DOM's object made from template.
- */
-
-function makeFragment(template, data, properties) {
-  var fragment = document.createDocumentFragment();
-
-  for (var i = 0; i < data.length; i++) {
+  function renderWizard(wizard) {
+    var template = document.querySelector('#similar-wizard-template')
+      .content
+      .querySelector('.setup-similar-item');
     var element = template.cloneNode(true);
 
-    for (var p in properties) {
-      if (properties[p] === 'name') {
-        element.querySelector(p).textContent = data[i][properties[p]];
-      } else {
-        element.querySelector(p).setAttribute('fill', data[i][properties[p]]);
-      }
-    }
-    fragment.appendChild(element);
+    element.querySelector('.setup-similar-label').textContent = wizard.name;
+    element.querySelector('.wizard-coat').setAttribute('fill', wizard.coatColors);
+    element.querySelector('.wizard-eyes').setAttribute('fill', wizard.eyesColors);
+
+    return element;
   }
-  return fragment;
-}
 
-// работа с данными
+  // работа с DOM
 
-var wizardList = [];
-for (var i = 0; i < NUMBER_OF_WIZARD; i++) {
-  wizardList.push(generateWizard());
-}
+  var setup = document.querySelector('.setup');
+  var charactersList = setup.querySelector('.setup-similar-list');
+  var similarCharacterBlock = setup.querySelector('.setup-similar');
 
-// работа с DOM
+  // create similar characters
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < NUMBER_OF_WIZARD; i++) {
+    fragment.appendChild(renderWizard(generateWizard()));
+  }
 
-var setup = document.querySelector('.setup');
-var charactersListElement = setup.querySelector('.setup-similar-list');
-var similarCharacterBlock = setup.querySelector('.setup-similar');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
-    .content
-    .querySelector('.setup-similar-item');
+  charactersList.appendChild(fragment);
+  similarCharacterBlock.classList.remove('hidden');
+})();
 
-// create similar characters
-
-setup.classList.remove('hidden');
-similarCharacterBlock.classList.remove('hidden');
-charactersListElement.appendChild(makeFragment(similarWizardTemplate, wizardList, PROPERTIES));
